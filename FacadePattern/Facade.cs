ccdce26c543f95300d99f9db1046085a3b52bab1
atkntuncer <atkntuncer@hotmail.com>
2023-09-14 18:17:17 +0300

    Added facade pattern

---
+++ FacadePattern/Facade.cs
@@ -0,0 +1,36 @@
+﻿using FacadePattern.Operations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadePattern
+{
+    public class Facade
+    {
+        private readonly OperationA _operationA;
+        private readonly OperationB _operationB;
+        private readonly OperationC _operationC;
+
+        public Facade(OperationA operationA , OperationB operationB, OperationC operationC)
+        {
+            _operationA = operationA;
+            _operationB = operationB;
+            _operationC = operationC;
+        }
+
+        public void OperateAandC()
+        {
+            _operationA.Operate();
+            _operationC.Operate();
+        }
+
+        public void FullOperation()
+        {
+            _operationA.Operate();
+            _operationB.Operate();
+            _operationC.Operate();
+        }
+    }
+}
---
+++ FacadePattern/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace FacadePattern
+{
+    //Provides simplified interface to complex systems
+    //I think this one is the most used unconciously among design patterns 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var facade = new Facade(new Operations.OperationA(), new Operations.OperationB(), new Operations.OperationC());
+
+            facade.OperateAandC();
+
+            Console.WriteLine();
+
+            facade.FullOperation();
+        }
+    }
+}
