bdff823905d6e68cae47ace328becd74ba53817c
atkntuncer <atkntuncer@hotmail.com>
2023-09-19 18:18:37 +0300

    Added interpreter pattern

---
+++ InterpreterPattern/InterpreterA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterpreterPattern
+{
+    public class InterpreterA : IInterpreter
+    {
+        public void Interpret(Context context)
+        {
+            context.Name = "Interpreted by Interpreter A";
+        }
+    }
+}
---
+++ InterpreterPattern/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace InterpreterPattern
+{
+    //Used for interpretation of input, change it to more understandable in the code
+    //For example SQL apps interpret queries
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new Context("Plain name");
+            Console.WriteLine(context.Name);
+            Console.WriteLine();
+
+            var interpreterA = new InterpreterA();
+            interpreterA.Interpret(context);
+            Console.WriteLine(context.Name);
+            Console.WriteLine();
+
+            var interpreterB = new InterpreterB();
+            interpreterB.Interpret(context);
+            Console.WriteLine(context.Name);
+            Console.WriteLine();
+        }
+    }
+}
